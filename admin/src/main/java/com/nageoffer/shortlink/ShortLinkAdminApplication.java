27b75d6f94bcce0c61972724fc7835034d695c8f
astryng <yong77721@gmail.com>
2026-01-31 17:17:53 +0800

    完成子模块结构构建

---
+++ admin/src/main/java/com/nageoffer/shortlink/ShortLinkAdminApplication.java
@@ -0,0 +1,12 @@
+package com.nageoffer.shortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
