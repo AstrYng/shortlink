f6e8c85a946d225561cf57d77e707a0ac058a8d0
astryng <yong77721@gmail.com>
2026-02-04 20:02:14 +0800

    用户登录拦截器忽略登录接口

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,14 +1,13 @@
 package com.nageoffer.shortlink.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
-import com.google.common.collect.Lists;
+import com.google.common.base.Objects;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * 用户信息传输过滤器
@@ -18,22 +17,19 @@ public class UserTransmitFilter implements Filter {
 
     private  final StringRedisTemplate stringRedisTemplate;
 
-    private static final List<String> IGNORE_URL = Lists.newArrayList(
-            "/api/short-link/v1/user/login",
-            "/api/short-link/v1/user/has-username"
-    );
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!Objects.equal(requestURI, "/api/short-link/v1/user/login")) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
         }
-
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
