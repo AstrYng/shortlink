97ea66be44024440a497cd43383a57610d123f4b
astryng <yong77721@gmail.com>
2026-02-27 21:41:31 +0800

    ……

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -1,20 +1,18 @@
 package com.nageoffer.shortlink.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+    public static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id` DESC) USING BTREE,\n" +
-            "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=2017943347453071363 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+            "  PRIMARY KEY (`id`),\n" +
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=2019026311641321475 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL) + "%n", i);
