aa1b3a5505bb0ab5f478cacf2fa28d08181cc75e
astryng <yong77721@gmail.com>
2026-03-06 21:42:36 +0800

    开发短链接监控之今日统计

--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,7 +2,10 @@ package com.nageoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.nageoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -10,6 +13,9 @@ import java.util.Date;
  * 短链接今日统计实体
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @TableName("t_link_stats_today")
 public class LinkStatsTodayDO extends BaseDO {
 
